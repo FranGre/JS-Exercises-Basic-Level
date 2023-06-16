34efeb47fc31d6cddc4b0c001b77a399b42862f1
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-06-16 23:04:23 +0200

    Exercise 1 Resolved

---
+++ Activity1/js/01-app.js
@@ -0,0 +1,38 @@
+const mayorEdad = 18;
+
+let edad = prompt('Que edad tienes?')
+
+
+
+try {
+    if (!isParseableToNumber(edad)){
+        throw new AgeTypeNotAllowed('Solo admite números');
+    }
+
+    canDrive(edad)
+    
+  } catch (e) {
+    console.log('Error',e.message)
+  }
+    
+
+function isParseableToNumber(numberStr){
+    return Number(numberStr) == numberStr;
+}
+
+function canDrive(edad){
+    if(isAdult(edad)){
+        document.write('Puedes conducir')
+    }else{
+        document.write('No puedes conducir')
+    }
+}
+
+function isAdult(edad){
+    return edad >= mayorEdad
+}
+
+function AgeTypeNotAllowed(message) {
+    const error = new Error(message);
+    return error;
+  }
